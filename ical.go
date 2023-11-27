5ef80e09bc578613b56ab66df71134a06c6a1683
revelaction <96388231+revelaction@users.noreply.github.com>
2023-11-27 16:13:04 +0100

    PoC golang-ical rrule-go

---
+++ ical.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"github.com/arran4/golang-ical"
+	"github.com/teambition/rrule-go"
+)
+
+func main() {
+	// Define the ICS file contents
+	icsData := []byte(`
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Your Company//Your Product//EN
+BEGIN:VEVENT
+UID:123456789
+DTSTART:20231225T000000Z
+DTEND:20231225T000000Z
+RRULE:FREQ=MONTHLY;BYMONTHDAY=25
+SUMMARY:Pay Rent
+DESCRIPTION:Remember to pay the rent.
+END:VEVENT
+END:VCALENDAR
+`)
+
+	// Parse the ICS file
+	cal, err := ics.ParseCalendar(strings.NewReader(string(icsData)))
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the events from the calendar
+	events := cal.Events()
+
+	// Print all recurring dates of the first event
+	if len(events) > 0 {
+		rule := events[0].GetProperty(ics.ComponentPropertyRrule).Value
+		start := events[0].GetProperty(ics.ComponentPropertyDtStart).Value
+        s, _ := rrule.StrToRRuleSet("DTSTART:" + start + "\nRRULE:" + rule)
+        fmt.Println("Date of the next event:", s.String())
+        //next := s.Iterator()
+
+        //for i := 0; i < 10; i++ {
+        //    time, _ := next()
+        //    fmt.Println("Date of the next event:", time)
+        //}
+        for _, t := range s.All() {
+            fmt.Println(t)
+        }
+	}
+}
+
