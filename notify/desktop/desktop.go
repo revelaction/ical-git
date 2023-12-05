f4bac8e68583706dc3e5cfdc273aac31fe79187d
revelaction <96388231+revelaction@users.noreply.github.com>
2023-12-05 12:16:55 +0100

    refactor notify

--- notify/desktop/desktop.go
+++ notify/desktop/desktop.go
@@ -17,6 +17,7 @@ func New(icon string) *Desktop {
 	}
 }
 
+// icon https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html#directory_layout
 func (d *Desktop) Notify(n notify.Notification) error {
 
 	beeep.Notify(n.Summary+n.EventTime.Format(time.RFC822), n.Description, "/usr/share/icons/hicolor/48x48/apps/filezilla.png")
--- notify/notify.go
+++ notify/notify.go
@@ -4,39 +4,21 @@ import (
 	"time"
 )
 
-type Type int
-
-const (
-	Telegram Type = iota
-	Email
-	LinuxDesktop
-)
 
 type Notifier interface {
 	Notify(n Notification) error
 }
 
-type Conf struct {
-	Type     string
-	Duration time.Duration
-}
-
-type Confs []Conf
 
 type Notification struct {
 	EventTime   time.Time
 	TimeZone    *time.Location
 	Summary     string
 	Description string
-	Type        Type
+	Type        string
 	Time time.Time
 }
 
-var configs = Confs{
-	{"telegram", 7 * 24 * time.Hour}, // 1 week
-	{"telegram", 24 * time.Hour},     // 1 day
-	{"desktop", 15 * time.Minute},    // 15 minutes
-}
 
 // notifier receives the potencial notification
 // generate possible notification accoreding to configs
