f6781b21f9628dc0d806418566a2e68e1f54ca95
revelaction <96388231+revelaction@users.noreply.github.com>
2023-12-08 10:56:48 +0100

    impl Desktop notification template

--- notify/desktop/desktop.go
+++ notify/desktop/desktop.go
@@ -4,7 +4,9 @@ import (
 	"github.com/gen2brain/beeep"
 	"github.com/revelaction/ical-git/config"
 	"github.com/revelaction/ical-git/notify"
-	"time"
+    "html/template"
+	//"time"
+	"bytes"
 )
 
 // Desktop implements the notify.Notifier interface
@@ -21,6 +23,33 @@ func New(conf config.Config) *Desktop {
 // icon https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html#directory_layout
 func (d *Desktop) Notify(n notify.Notification) error {
 
-	beeep.Notify(n.Summary, "<b>"+n.EventTime.Format(time.RFC822)+"</b>"+"\n"+n.Description, d.config.Desktop.Icon)
+    body, err:= renderNotification(n)
+    if err != nil {
+        return err
+    }
+
+	beeep.Notify(n.Summary, body, d.config.Desktop.Icon)
 	return nil
 }
+
+
+func renderNotification(n notify.Notification) (string, error) {
+    const tpl = `
+📅 {{.EventTime.Format "Monday, 2006-02-01"}} {{.EventTime.Format "🕒 15:04"}} 
+🌍 Time Zone: {{.TimeZone}}
+📢 Summary: <b>{{.Summary}}</b>
+📝 Description: {{.Description}}
+`
+    t, err := template.New("notification").Parse(tpl)
+    if err != nil {
+        return "", err
+    }
+
+    var buf bytes.Buffer
+    if err := t.Execute(&buf, n); err != nil {
+        return "", err
+    }
+
+    return buf.String(), nil
+}
+
