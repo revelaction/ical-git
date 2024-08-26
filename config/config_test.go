64f839e7d5f81a071e957e1cdaa10df24558dad0
revelaction <96388231+revelaction@users.noreply.github.com>
2024-08-27 00:48:09 +0200

    test: Refactor test cases and add new test for alarm filtering

--- config/config_test.go
+++ config/config_test.go
@@ -7,7 +7,7 @@ import (
 
 
 func TestAlarmsAllowedDesktop(t *testing.T) {
-	const testToml = []byte(`
+	var testToml = []byte(`
 notifiers = ["desktop"]
 alarms = [
 	{type = "desktop", when = "-P1D"},  
@@ -20,25 +20,15 @@ alarms = [
 		t.Fatalf("Failed to load config: %v", err)
 	}
 
-	expectedAlarms := []Alarm{
-		{Type: "desktop", When: "-P1D"},
-		{Type: "desktop", When: "-PT15M"},
-		{Type: "desktop", When: "-PT1H"},
-	}
-
 	actualAlarms := conf.AlarmsAllowed()
 
-	if len(actualAlarms) != len(expectedAlarms) {
-		t.Fatalf("Expected %d alarms, got %d", len(expectedAlarms), len(actualAlarms))
-	}
-
-	if len(actualAlarms) != len(expectedAlarms) {
-		t.Fatalf("Expected %d alarms, got %d", len(expectedAlarms), len(actualAlarms))
+	if len(actualAlarms) != expectedLen {
+		t.Fatalf("Expected %d alarms, got %d", expectedLen, len(actualAlarms))
 	}
 }
 
 func TestAlarmsAllowedTelegram(t *testing.T) {
-	const testToml = []byte(`
+	var testToml = []byte(`
 notifiers = ["telegram"]
 alarms = [
 	{type = "telegram", when = "-P1D"},  
@@ -51,21 +41,34 @@ alarms = [
 		t.Fatalf("Failed to load config: %v", err)
 	}
 
-	expectedAlarms := []Alarm{
-		{Type: "telegram", When: "-P1D"},
-		{Type: "telegram", When: "-PT15M"},
-		{Type: "telegram", When: "-PT1H"},
-	}
-
+    expectedLen := 3
 	actualAlarms := conf.AlarmsAllowed()
 
-	if len(actualAlarms) != len(expectedAlarms) {
-		t.Fatalf("Expected %d alarms, got %d", len(expectedAlarms), len(actualAlarms))
+	if len(actualAlarms) != expectedLen {
+		t.Fatalf("Expected %d alarms, got %d", expectedLen, len(actualAlarms))
 	}
 
-	for i, alarm := range actualAlarms {
-		if alarm != expectedAlarms[i] {
-			t.Fatalf("Expected alarm %v, got %v", expectedAlarms[i], alarm)
-		}
+}
+
+func TestAlarmsAllowedNoTelegram(t *testing.T) {
+	var testToml = []byte(`
+notifiers = ["desktop"]
+alarms = [
+	{type = "telegram", when = "-P1D"},  
+	{type = "telegram", when = "-PT15M"},  
+	{type = "telegram", when = "-PT1H"},  
+]
+`)
+	conf, err := Load(testToml)
+	if err != nil {
+		t.Fatalf("Failed to load config: %v", err)
 	}
+
+    expectedLen := 0
+	actualAlarms := conf.AlarmsAllowed()
+
+	if len(actualAlarms) != expectedLen {
+		t.Fatalf("Expected %d alarms, got %d", expectedLen, len(actualAlarms))
+	}
+
 }
