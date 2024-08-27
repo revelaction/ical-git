38846c8f187591d604a83f136e97f1bf8a1b117b
revelaction <96388231+revelaction@users.noreply.github.com>
2024-08-27 23:10:04 +0200

    refactor: simplify test configuration and update event dates

--- ical/parse_test.go
+++ ical/parse_test.go
@@ -18,30 +18,15 @@ alarms = [
 	{type = "desktop", when = "-P1D"},  
 	{type = "desktop", when = "-PT15M"},  
 	{type = "desktop", when = "-PT1H"},  
-	{type = "desktop", when = "-P5D"}, 
-	{type = "desktop", when = "-P6D"}, 
-	{type = "telegram", when = "-P4D"}, 
-	{type = "desktop", when = "-P2DT22H49M"}, 
-	{type = "desktop", when = "-P3D"}, 
 ]
 
-#notifiers = ["telegram", "desktop"]
 notifiers = ["desktop"]
-
-[fetcher_filesystem]
-directory = "testdata"
-
-[notifier_telegram]
-token = "yuu3b3k"
-chat_id = 588488
-
-[notifier_desktop]
-icon = "/usr/share/icons/hicolor/48x48/apps/filezilla.png"
 `)
 	conf, err := config.Load(configData)
 	if err != nil {
 		t.Fatalf("Failed to load config: %v", err)
 	}
+
 	start := time.Now()
 	parser := NewParser(conf, start)
 
@@ -51,14 +36,13 @@ BEGIN:VCALENDAR
 VERSION:2.0
 BEGIN:VEVENT
 SUMMARY:Simple Event
-DTSTART:20231211T100000
-DTEND:20231211T103000
+DTSTART:20241201T100000Z
 END:VEVENT
 END:VCALENDAR
-	`)
+`)
 
 	// Parse the iCal data
-	err := parser.Parse(icalData)
+	err = parser.Parse(icalData)
 	if err != nil {
 		t.Fatalf("Unexpected error: %v", err)
 	}
