481b49a2a798f7650c1a7346cd05a627fe521372
revelaction <96388231+revelaction@users.noreply.github.com>
2024-09-02 21:44:39 +0200

    fix: correct typo in test assertion

--- ical/parse_test.go
+++ ical/parse_test.go
@@ -125,7 +125,7 @@ END:VCALENDAR
 
 	t.Logf("notifications: %#v", notifications)
 	notification := notifications[0]
-	if notification.Summary != "Event with Alarms" {
+	if notification. != "Event with Alarms" {
 		t.Errorf("Expected summary 'Event with Alarms', got '%s'", notification.Summary)
 	}
 }
