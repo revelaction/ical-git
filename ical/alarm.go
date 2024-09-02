e5f297b5c30e26305be47eac792f8d248ec7b42d
revelaction <96388231+revelaction@users.noreply.github.com>
2024-09-02 18:42:45 +0200

    refactor: optimize alarm retrieval and handling in getEventAlarm

--- ical/alarm.go
+++ ical/alarm.go
@@ -7,14 +7,17 @@ import (
 
 func getEventAlarm(event *ics.VEvent) []alarm.Alarm {
 	// Retrieve the alarms from the event
+    var result =[]alarm.Alarm{} 
 	alarms := event.Alarms()
-	result := make([]alarm.Alarm, 0, len(alarms))
+	if len(alarms) == 0 {
+		return result
+	}
 
 	for _, a := range alarms {
 		triggerProp := a.GetProperty(ics.ComponentPropertyTrigger)
 		actionProp := a.GetProperty(ics.ComponentPropertyAction)
 
-		result = append(result, alarm.Alarm{
+        result = append(result, alarm.Alarm{
 			Action:     actionProp.Value,
 			DurIso8601: triggerProp.Value,
 		})
